dddc30f9b1954f628a9373689c61fad381f0e363
Alice Zhao <azhao991@gmail.com>
2022-02-19 18:35:19 -0800

    feat: hook up graphql client to make queries and mutation

--- src/pages/incidents/index.tsx
+++ src/pages/incidents/index.tsx
@@ -3,6 +3,8 @@ import { Component } from 'solid-js'
 import IncidentsSidebar from '../../components/IncidentsSidebar'
 import IncidentsTable from '../../components/IncidentsTable'
 import AppLayout from '../../components/layouts/AppLayout'
+import { Show } from 'solid-js'
+import { createQuery } from 'solid-urql'
 
 const Incidents: Component = () => {
   return (
