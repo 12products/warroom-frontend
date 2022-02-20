0d18082ad679e1fc30ef1e71eb79e1cf1ffb573c
Alice Zhao <azhao991@gmail.com>
2022-02-19 18:54:30 -0800

    chore: remove unused imports

--- src/pages/incidents/index.tsx
+++ src/pages/incidents/index.tsx
@@ -3,8 +3,6 @@ import { Component } from 'solid-js'
 import IncidentsSidebar from '../../components/IncidentsSidebar'
 import IncidentsTable from '../../components/IncidentsTable'
 import AppLayout from '../../components/layouts/AppLayout'
-import { Show } from 'solid-js'
-import { createQuery } from 'solid-urql'
 
 const Incidents: Component = () => {
   return (
