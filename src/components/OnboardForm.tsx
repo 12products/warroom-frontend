b5be4ec868e09f06a68560460176c6fc47bd7ce6
Faith Morante <faith.morante@gmail.com>
2022-02-25 06:33:49 +0800

    remove console logs

--- src/components/OnboardForm.tsx
+++ src/components/OnboardForm.tsx
@@ -60,10 +60,7 @@ const CreateServiceForm: Component = () => {
             name: organizationName,
           },
         })
-        console.log('test', {
-          error1: createUserResult().error,
-          error2: createOrganizationResult().error,
-        })
+
         if (createUserResult().error || createOrganizationResult().error) {
           alert('Something went wrong, please try again.')
           return
